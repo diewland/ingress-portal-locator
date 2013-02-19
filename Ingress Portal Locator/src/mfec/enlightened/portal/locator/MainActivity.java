6bbdd8ccbe3edd4b7c73fec92c0feaff88c8df3d
diewland <diewland@gmail.com>
2013-02-19 20:10:18 +0700

    Add "Send To" filter. Move some util function to Utility class.

--- Ingress Portal Locator/src/mfec/enlightened/portal/locator/MainActivity.java
+++ Ingress Portal Locator/src/mfec/enlightened/portal/locator/MainActivity.java
@@ -2,7 +2,6 @@ package mfec.enlightened.portal.locator;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -11,7 +10,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.location.Location;
 import android.location.LocationManager;
 import android.media.ExifInterface;
 import android.net.Uri;
@@ -41,128 +39,64 @@ public class MainActivity extends Activity {
 	private File selectedFile;
 	private static final int SELECT_PHOTO = 100;
 	private Builder layerBuilder;
-	
-	double convertToDegree(String stringDMS,String ref){
-		//from http://android-er.blogspot.com/2010/01/convert-exif-gps-info-to-degree-format.html
-		Double result = null;
+
+	private void loadImageToMap(Uri selectedImg){
+		Cursor csr = getContentResolver().query(selectedImg, new String[]{android.provider.MediaStore.Images.ImageColumns.DATA}, null, null, null);
+		csr.moveToFirst();
 		
-		Log.d("maplat", stringDMS);
-		String[] DMS = stringDMS.split(",", 3);
-		String[] stringD = DMS[0].split("/", 2);
-		Double D0 = new Double(stringD[0]);
-		Double D1 = new Double(stringD[1]);
-		Double FloatD = D0/D1;
-
-		String[] stringM = DMS[1].split("/", 2);
-		Double M0 = new Double(stringM[0]);
-		Double M1 = new Double(stringM[1]);
-		Double FloatM = M0/M1;
-		  
-		String[] stringS = DMS[2].split("/", 2);
-		Double S0 = new Double(stringS[0]);
-		Double S1 = new Double(stringS[1]);
-		Double FloatS = S0/S1;
-		  
-		result = new Double(FloatD + (FloatM/60) + (FloatS/3600));
-		if(ref.equals("W") || ref.equals("S")){
-			result *= -1;
+		Log.d("imglog",csr.getString(0));
+		selectedUri = csr.getString(0);
+		try {
+			ExifInterface exif = new ExifInterface(selectedUri);
+			String dmslat = exif.getAttribute(exif.TAG_GPS_LATITUDE);
+			String dmslatref = exif.getAttribute(exif.TAG_GPS_LATITUDE_REF);
+			String dmslong = exif.getAttribute(exif.TAG_GPS_LONGITUDE);
+			String dmslongref = exif.getAttribute(exif.TAG_GPS_LONGITUDE_REF);
+			if(dmslat != null){
+				map_lat_ori = Utility.convertToDegree(dmslat, dmslatref);
+				map_long_ori = Utility.convertToDegree(dmslong, dmslongref);
+				map_lat = map_lat_ori;
+				map_long = map_long_ori;
+				moveToLatLong();
+				Log.d("maplat", "Got Photo LOC: " + map_lat + " - " + map_long);
+			}else{
+				Log.d("maplat","Null TAG");
+			}
+		} catch (IOException e) {
+			Toast t = Toast.makeText(getApplicationContext(), "Error Open Image", Toast.LENGTH_SHORT);
+			t.show();
 		}
-		return result;
+		
+		// update filename
+		selectedFile = new File(selectedUri);
+		getActionBar().setTitle(selectedFile.getName());
+		/*
+		try { // ( API LV14 - ICS )
+			//
+			// 1 - not work
+			// getActionBar().setIcon(getDrawableFromUrl(getResources(), selectedUri));
+			//
+			// 2 - not work
+			// getActionBar().setLogo(getDrawableFromUrl(getResources(), selectedUri));
+			//
+			// 3 - not work
+			// ImageView logo = (ImageView) findViewById(android.R.id.home);
+			// logo.setImageDrawable(getDrawableFromUrl(getResources(), selectedUri));
+			//
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		*/
 	}
-	
-	String convertToDMS(double coord){
-		String[] dms = Location.convert(coord, Location.FORMAT_SECONDS).split(":");
-		String deg = dms[0];
-		String min = dms[1];
-		String[] secs = dms[2].split("\\.");
-		String sec = secs[0]+secs[1];
-		String ret = deg + "/1," + min + "/1," + sec + "/100000";
-		return ret;
-	}
-	
-	/*
-	// http://stackoverflow.com/questions/3375166/android-drawable-images-from-url
-	Drawable getDrawableFromUrl(Resources res, String url) throws java.net.MalformedURLException, java.io.IOException {
-	    Bitmap x;
 
-	    HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-	    connection.connect();
-	    InputStream input = connection.getInputStream();
-
-	    x = BitmapFactory.decodeStream(input);
-	    return new BitmapDrawable(res, x);
-	}
-	*/
-	
+	// response to Gallery Picker
 	protected void onActivityResult(int requestCode, int resultCode, Intent data){
 		super.onActivityResult(requestCode, resultCode, data);
 		
 		if(requestCode == SELECT_PHOTO && resultCode == RESULT_OK){
 			Uri selectedImg = data.getData();
-			Cursor csr = getContentResolver().query(selectedImg, new String[]{android.provider.MediaStore.Images.ImageColumns.DATA}, null, null, null);
-			csr.moveToFirst();
-			
-			Log.d("imglog",csr.getString(0));
-			selectedUri = csr.getString(0);
-			try {
-				ExifInterface exif = new ExifInterface(selectedUri);
-				String dmslat = exif.getAttribute(exif.TAG_GPS_LATITUDE);
-				String dmslatref = exif.getAttribute(exif.TAG_GPS_LATITUDE_REF);
-				String dmslong = exif.getAttribute(exif.TAG_GPS_LONGITUDE);
-				String dmslongref = exif.getAttribute(exif.TAG_GPS_LONGITUDE_REF);
-				if(dmslat != null){
-					map_lat_ori = convertToDegree(dmslat, dmslatref);
-					map_long_ori = convertToDegree(dmslong, dmslongref);
-					map_lat = map_lat_ori;
-					map_long = map_long_ori;
-					moveToLatLong();
-					Log.d("maplat", "Got Photo LOC: " + map_lat + " - " + map_long);
-				}else{
-					Log.d("maplat","Null TAG");
-				}
-			} catch (IOException e) {
-				Toast t = Toast.makeText(getApplicationContext(), "Error Open Image", Toast.LENGTH_SHORT);
-				t.show();
-			}
-			
-			// update filename
-			selectedFile = new File(selectedUri);
-			getActionBar().setTitle(selectedFile.getName());
-			/*
-			try { // ( API LV14 - ICS )
-				//
-				// 1 - not work
-				// getActionBar().setIcon(getDrawableFromUrl(getResources(), selectedUri));
-				//
-				// 2 - not work
-				// getActionBar().setLogo(getDrawableFromUrl(getResources(), selectedUri));
-				//
-				// 3 - not work
-				// ImageView logo = (ImageView) findViewById(android.R.id.home);
-				// logo.setImageDrawable(getDrawableFromUrl(getResources(), selectedUri));
-				//
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			*/
+			loadImageToMap(selectedImg);
 		}
-	}
-	
-	private double[] getLocation(){
-		LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		List<String> providers = lm.getAllProviders();
-		Location l = null;
-		double[] ret = new double[2];
-		for(int i = providers.size() -1; i >= 0; i--){
-			l = lm.getLastKnownLocation(providers.get(i));
-			if(l != null) break;
-		}
-		if(l != null){
-			ret[0] = l.getLatitude();
-			ret[1] = l.getLongitude();
-			return ret;
-		}
-		return null;
 	}
 	
 	@Override
@@ -175,16 +109,15 @@ public class MainActivity extends Activity {
 	
 		setContentView(R.layout.activity_main);
 		
+		setUpMapIfNeeded();
+		
 		// Default Location from coarse location
-		double[] bestloc = getLocation();
+		LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		double[] bestloc = Utility.getLocation(lm);
 		if(bestloc != null){
 			map_lat_ori  = map_lat  = bestloc[0];
 			map_long_ori  = map_long  = bestloc[1];
 		}
-		
-		Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-		photoPickerIntent.setType("image/jpeg");
-		startActivityForResult(photoPickerIntent, SELECT_PHOTO);
 		
 		// setup layer dialog
 		layerBuilder = new AlertDialog.Builder(this);
@@ -208,11 +141,20 @@ public class MainActivity extends Activity {
 						}
 					});
 		
-		// init
-		setUpMapIfNeeded();
-		
-		// move to lat, long
-		moveToLatLong();
+	    Intent intent = getIntent();
+	    Bundle extras = intent.getExtras();
+	    String action = intent.getAction();
+		if(Intent.ACTION_SEND.equals(action)){ // come from Send to menu
+			if(extras.containsKey(Intent.EXTRA_STREAM)){
+				Uri uri = (Uri) extras.getParcelable(Intent.EXTRA_STREAM);
+				loadImageToMap(uri);
+			}
+		}
+		else { // come from app icon
+			Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+			photoPickerIntent.setType("image/jpeg");
+			startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+		}
 	}
 	
 	private void setUpMapIfNeeded() {
@@ -287,9 +229,9 @@ public class MainActivity extends Activity {
 			Double curr_long = ll[1];
 			try {
 				ExifInterface exif = new ExifInterface(selectedUri);
-				String str_lat = convertToDMS(map_lat);
+				String str_lat = Utility.convertToDMS(map_lat);
 				String lat_ref = map_lat > 0 ? "N" : "E";
-				String str_long = convertToDMS(map_long);
+				String str_long = Utility.convertToDMS(map_long);
 				String long_ref = map_long > 0 ? "E" : "W";
 				exif.setAttribute(exif.TAG_GPS_LATITUDE, str_lat);
 				exif.setAttribute(exif.TAG_GPS_LATITUDE_REF, lat_ref);
---
+++ Ingress Portal Locator/src/mfec/enlightened/portal/locator/Utility.java
@@ -0,0 +1,79 @@
+package mfec.enlightened.portal.locator;
+
+import java.util.List;
+
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+
+public class Utility {
+
+	public static double convertToDegree(String stringDMS,String ref){
+		//from http://android-er.blogspot.com/2010/01/convert-exif-gps-info-to-degree-format.html
+		Double result = null;
+		
+		Log.d("maplat", stringDMS);
+		String[] DMS = stringDMS.split(",", 3);
+		String[] stringD = DMS[0].split("/", 2);
+		Double D0 = new Double(stringD[0]);
+		Double D1 = new Double(stringD[1]);
+		Double FloatD = D0/D1;
+
+		String[] stringM = DMS[1].split("/", 2);
+		Double M0 = new Double(stringM[0]);
+		Double M1 = new Double(stringM[1]);
+		Double FloatM = M0/M1;
+		  
+		String[] stringS = DMS[2].split("/", 2);
+		Double S0 = new Double(stringS[0]);
+		Double S1 = new Double(stringS[1]);
+		Double FloatS = S0/S1;
+		  
+		result = new Double(FloatD + (FloatM/60) + (FloatS/3600));
+		if(ref.equals("W") || ref.equals("S")){
+			result *= -1;
+		}
+		return result;
+	}
+	
+	public static String convertToDMS(double coord){
+		String[] dms = Location.convert(coord, Location.FORMAT_SECONDS).split(":");
+		String deg = dms[0];
+		String min = dms[1];
+		String[] secs = dms[2].split("\\.");
+		String sec = secs[0]+secs[1];
+		String ret = deg + "/1," + min + "/1," + sec + "/100000";
+		return ret;
+	}
+	
+	public static double[] getLocation(LocationManager lm){
+		List<String> providers = lm.getAllProviders();
+		Location l = null;
+		double[] ret = new double[2];
+		for(int i = providers.size() -1; i >= 0; i--){
+			l = lm.getLastKnownLocation(providers.get(i));
+			if(l != null) break;
+		}
+		if(l != null){
+			ret[0] = l.getLatitude();
+			ret[1] = l.getLongitude();
+			return ret;
+		}
+		return null;
+	}
+	
+	/*
+	// http://stackoverflow.com/questions/3375166/android-drawable-images-from-url
+	public static Drawable getDrawableFromUrl(Resources res, String url) throws java.net.MalformedURLException, java.io.IOException {
+	    Bitmap x;
+
+	    HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+	    connection.connect();
+	    InputStream input = connection.getInputStream();
+
+	    x = BitmapFactory.decodeStream(input);
+	    return new BitmapDrawable(res, x);
+	}
+	*/
+	
+}
